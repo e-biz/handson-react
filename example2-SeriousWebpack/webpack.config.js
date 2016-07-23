87a72e366d08c0c5df831a7d0b0b6a4b839d4afa
Jean-Baptiste Petit <beanbaptiste.petit@gmail.com>
2016-07-23 12:59:42 +0200

    finish exercice 2, prod ready !

--- example2-SeriousWebpack/webpack.config.js
+++ example2-SeriousWebpack/webpack.config.js
@@ -2,6 +2,12 @@ var TransferWebpackPlugin = require('transfer-webpack-plugin');
 var path = require('path');
 var webpack = require('webpack');
 
+var NODE_ENV = 'dev';
+
+if (NODE_ENV === 'production') {
+    // Do some cool stuff like uglify, minify...
+}
+
 module.exports = {
     entry: {
         app: ['./app/js/main.js']
